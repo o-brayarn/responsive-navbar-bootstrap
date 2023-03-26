1545095f90a5fe2177edfd9dcac1aa796cb02aec
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-03-26 14:13:40 +0300

    menuData in the NavBar and bootstrap path

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,49 @@
 import React from 'react'
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, NavLink } from "react-bootstrap";
 
 export default function NavBar() {
+    const menuData = [
+        {
+            path: '/about',
+            name: "ABOUT"
+        },
+       
+        {
+            path: '/services',
+            name: "SERVICES"
+        },
+         {
+            path: '/skills',
+            name: "SKILLS"
+        },
+        {
+            path: '/experience',
+            name: "EXPERIENCE"
+        },
+         {
+            path: '/contact',
+            name: "PROJECTS"
+        },
+         {
+            path: '/contact',
+            name: "CONTACT"
+        }
+    ]
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar className="navbar" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+        <Navbar.Brand className="brand" href="#home">Logo</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav>
+          <Nav className="ms-auto flex-grow-1 justify-content-evenly">
+           { menuData.map((item)=>(
+               <NavLink to={item.path} key={item.name} >
+                   <div className="list-item">{item.name}</div>
+               </NavLink>
+           ))
+            }
+                  </Nav>
+                  {/* <Nav className='me-auto'></Nav> */}
         </Navbar.Collapse>
       </Container>
     </Navbar>
